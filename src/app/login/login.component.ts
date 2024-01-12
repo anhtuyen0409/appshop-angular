c5fe24efd8651ecb69f5b69340b8264997cba8a9
anhtuyen0409 <nguyenanhtuyen10a5@gmail.com>
2024-01-12 18:07:30 +0700

    call api login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
+import { LoginDTO } from '../dtos/user/login.dto';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  @ViewChild('loginForm') loginForm!: NgForm;
 
-  ngOnInit(): void {
+  phone: string;
+  password: string;
+
+  constructor(private router: Router, private userService: UserService) {
+    this.phone = '';
+    this.password = '';
+  }
+
+  onPhoneChange() {
+    console.log(`Phone typed: ${this.phone}`);
+  }
+
+  login() {
+
+    debugger
+
+    const loginDTO: LoginDTO = {
+      "phone_number": this.phone,
+      "password": this.password
+    }
+
+    this.userService.login(loginDTO).subscribe({
+      next: (response: any) => {
+          debugger
+          // login thành công, chuyển sang màn hình sản phẩm
+          //this.router.navigate(['/product']);
+      },
+      complete: () => {
+        debugger
+      },
+      error: (error: any) => {
+        // xử lý lỗi nếu có
+        alert(`Đăng nhập không thành công. Lỗi: ${error.error}`);
+      }
+    });
+
   }
 
 }
