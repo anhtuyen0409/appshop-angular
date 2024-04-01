ad90f3c152c9de58587a0c511c9728e6acd8f6e1
anhtuyen0409 <nguyenanhtuyen10a5@gmail.com>
2024-04-01 17:21:08 +0700

    user detail

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,13 @@
 import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
 import { LoginDTO } from '../../dtos/user/login.dto';
 import { TokenService } from 'src/app/services/token.service';
 import { LoginResponse } from 'src/app/responses/user/login.response';
 import { RoleService } from 'src/app/services/role.service';
 import { Role } from 'src/app/models/role';
+import { UserResponse } from 'src/app/responses/user/user.response';
 
 @Component({
   selector: 'app-login',
@@ -17,17 +18,20 @@ export class LoginComponent {
 
   @ViewChild('loginForm') loginForm!: NgForm;
 
-  phone: string;
-  password: string;
+  phone: string = '';
+  password: string = '';
 
   roles: Role[] = []; // mảng role
   rememberMe: boolean = true;
   selectedRole: Role | undefined; //biến để lưu giá trị được chọn từ dropdown
+  userResponse?: UserResponse;
 
-  constructor(private router: Router, private userService: UserService, private tokenService: TokenService, private roleService: RoleService) {
-    this.phone = '';
-    this.password = '';
-  }
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private userService: UserService,
+    private tokenService: TokenService,
+    private roleService: RoleService) {}
 
   ngOnInit() {
     // gọi api lấy danh sách roles và lưu vào biến roles
@@ -62,14 +66,38 @@ export class LoginComponent {
 
     this.userService.login(loginDTO).subscribe({
       next: (response: LoginResponse) => {
-          debugger
-          const {token} = response;
-          if(this.rememberMe) {
-            this.tokenService.setToken(token);
-          }
-
-          // login thành công, chuyển sang màn hình sản phẩm
-          //this.router.navigate(['/product']);
+        debugger
+        const {token} = response;
+        if(this.rememberMe) {
+          this.tokenService.setToken(token);
+          debugger;
+          this.userService.getUserDetails(token).subscribe({
+            next: (response: any) => {
+              debugger
+              this.userResponse = {
+                id: response.id,
+                fullname: response.fullname,
+                address: response.address,
+                is_active: response.is_active,
+                date_of_birth: new Date(response.date_of_birth),
+                facebook_account_id: response.facebook_account_id,
+                google_account_id: response.google_account_id,
+                role: response.role
+              }
+              // lưu user vào local storage
+              this.userService.saveUserResponseToLocalStorage(this.userResponse);
+              // login thành công, chuyển sang trang home
+              this.router.navigate(['/']);
+            },
+            complete: () => {
+              debugger
+            },
+            error: (error: any) => {
+              debugger
+              alert(error.error.message);
+            }
+          })
+        }
       },
       complete: () => {
         debugger
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -5,6 +5,7 @@ import { RegisterDTO } from '../dtos/user/register.dto';
 import { LoginDTO } from '../dtos/user/login.dto';
 import { enviroment } from '../environments/environment';
 import { HttpUtilService } from './http.util.service';
+import { UserResponse } from '../responses/user/user.response';
 
 
 @Injectable({
@@ -14,6 +15,7 @@ export class UserService {
 
   private apiRegister = `${enviroment.apiBaseUrl}/users/register`;
   private apiLogin = `${enviroment.apiBaseUrl}/users/login`;
+  private apiUserDetails =`${enviroment.apiBaseUrl}/users/details`;
 
   private apiConfig = {
     headers: this.httpUtilService.createHeaders(),
@@ -27,6 +29,47 @@ export class UserService {
 
   login(loginDTO: LoginDTO):Observable<any> {
     return this.http.post(this.apiLogin, loginDTO, this.apiConfig);
+  }
+
+  getUserDetails(token: string) {
+    return this.http.post(this.apiUserDetails, {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      })
+    })
+  }
+
+  saveUserResponseToLocalStorage(userResponse?: UserResponse) {
+    try {
+      if(userResponse == null || !userResponse) {
+        return;
+      }
+      // convert userResponse object to JSON string
+      const userResponseJSON = JSON.stringify(userResponse);
+      // save JSON string to local storage with a key
+      localStorage.setItem('userResponse', userResponseJSON);
+      console.log('User response saved to local storage')
+    } catch (error) {
+      console.error('Error Saving user response to local storage', error);
+    }
+  }
+
+  getUserResponseFromLocalStorage() {
+    try {
+      // retrieve JSON string from local storage using key
+      const userResponseJSON = localStorage.getItem('userResponse');
+      if(userResponseJSON == null || userResponseJSON == undefined) {
+        return null;
+      }
+      // convert JSON string back to an object
+      const userResponse = JSON.parse(userResponseJSON!);
+      console.log('User response retrieved from local storage');
+      return userResponse;
+    } catch (error) {
+      console.error('Error retrieving user response from local storage', error);
+      return null;
+    }
   }
 
 }
